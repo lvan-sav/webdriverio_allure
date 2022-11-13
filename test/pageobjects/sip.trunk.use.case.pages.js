d6b3d6168155041c5586695fb9ba318ba9acfb28
lvansav <vansav.ka@gmail.com>
2022-11-13 05:14:21 +0200

    Add tests for sip trunking use case pages

---
+++ test/pageobjects/sip.trunk.use.case.pages.js
@@ -0,0 +1,118 @@
+const { Page } = require("./page");
+
+class UcaasCasePage extends Page {
+    pageTitleLoc = 'h1 strong'
+    purchaseNumLinkLoc = '[href*="api/v2/numbers"]'
+    exploreVideoLinkLoc = '[href*="api/v2/video"]'
+    exploreReportsLinkLoc = '[href*="api/v1/reports"]'
+    startFreeBtnLoc = 'main div:nth-child(2)>div>[href="/sign-up"]'
+
+    async pageTitle () {
+        return await $(this.pageTitleLoc)
+    }
+
+    async purchaseNumLink () {
+        return await $(this.purchaseNumLinkLoc)
+    }
+
+    async exploreVideoLink () {
+        return await $(this.exploreVideoLinkLoc)
+    }
+
+    async exploreReportsLink () {
+        return await $(this.exploreReportsLinkLoc)
+    }
+
+    async startFreeBtn () {
+        return await $(this.startFreeBtnLoc)
+    }
+}
+
+class ManagedServCasePage extends Page {
+    pageTitleLoc = 'h1>span'
+    monUsagePageSubtitleLoc = 'div:nth-child(6) h2'
+    signUpTodayBtnLoc = 'div:nth-child(3) [href="/sign-up"]'
+    seeApiPricingLinkLoc = 'main [href="/pricing/call-control"]'
+
+    async pageTitle () {
+        return await $(this.pageTitleLoc)
+    }
+
+    async monUsePageSubtitle () {
+        return await $(this.monUsagePageSubtitleLoc)
+    }
+
+    async signUpTodayBtn () {
+        return await $(this.signUpTodayBtnLoc)
+    }
+
+    async seeApiPricingLink () {
+        return await $(this.seeApiPricingLinkLoc)
+    }
+}
+
+class SmallBussCasePage extends Page {
+    pageTitleLoc = 'h1>span'
+    voipVsLandlinePageSubtitleLoc = 'div:nth-child(3) header h2'
+    letsTalkBtnLoc = 'main [href="/contact-us"]'
+    signUpFor10BtnLoc = 'div:nth-child(10) [href="/sign-up"]'
+
+    async pageTitle () {
+        return await $(this.pageTitleLoc)
+    }
+
+    async voipVsLandlinePageSubtitle () {
+        return await $(this.voipVsLandlinePageSubtitleLoc)
+    }
+
+    async letsTalkBtn () {
+        return await $(this.letsTalkBtnLoc)
+    }
+
+    async signUpFor10Btn () {
+        return await $(this.signUpFor10BtnLoc)
+    }
+}
+
+class MultiCloudCasePage extends Page {
+    pageTitleLoc = 'h1>span'
+    exploreDocsLinkLoc = '[href*="api/v1/networking"]'
+    downloadSheetLinkLoc = 'main [href*="image"]'
+    tryFreeBtnLoc = 'div:nth-child(14) [href="/sign-up"]'
+    buildProd = {
+        firstHalf: 'main li:nth-child(',
+        secHalf: ') [href*="products"]'
+    }
+
+    async pageTitle () {
+        return await $(this.pageTitleLoc)
+    }
+
+    async exploreDocLink () {
+        return await $(this.exploreDocsLinkLoc)
+    }
+
+    async downloadSheetLink () {
+        return await $(this.downloadSheetLinkLoc)
+    }
+
+    async tryFreeBtn () {
+        return await $(this.tryFreeBtnLoc)
+    }
+
+    async buildProduct (prodIndex) {
+        let buildProd = `${this.buildProd.firstHalf}${prodIndex}${this.buildProd.secHalf}`
+        return await $(buildProd)
+    }
+}
+
+module.exports = {
+    UcaasCasePage: UcaasCasePage,
+    ucaasCasePage: new UcaasCasePage,
+    ManagedServCasePage: ManagedServCasePage,
+    managedServCasePage: new ManagedServCasePage,
+    SmallBussCasePage: SmallBussCasePage,
+    smallBussCasePage: new SmallBussCasePage,
+    MultiCloudCasePage: MultiCloudCasePage,
+    multiCloudCasePage: new MultiCloudCasePage
+}
---
+++ test/specs/sip.trunking.open.use.cases.js
@@ -0,0 +1,66 @@
+const { mainPage } = require("../pageobjects/main.page");
+const { sipTrunkingProdPage } = require("../pageobjects/sip.trunk.prod.page");
+const { ucaasCasePage, 
+        managedServCasePage,
+        smallBussCasePage,
+        multiCloudCasePage } = require('../pageobjects/sip.trunk.use.case.pages')
+
+describe('Opening use cases test suite for the SIP Trunking page', () => {
+    beforeEach( async () => {
+        mainPage.headSipTrunkLinkClick()
+    });
+
+    it('Should to open the UCaaS use case page by clicking the UCaaS use \
+ case block in the SIP Trunking page', async () => {
+        await sipTrunkingProdPage.openUcaasUseCase()
+
+        await expect(browser).toHaveUrlContaining(/use-cases\/ucaas$/)
+        await expect(ucaasCasePage.pageTitle()).toBeDisplayed()
+        await expect(ucaasCasePage.pageTitle()).toHaveText('UCaaS')
+        await expect(ucaasCasePage.purchaseNumLink()).toBeClickable()
+        await expect(ucaasCasePage.exploreVideoLink()).toBeClickable()
+        await expect(ucaasCasePage.exploreReportsLink()).toBeClickable()
+        await expect(ucaasCasePage.startFreeBtn()).toBeClickable()
+    });
+
+    it('Should to open the Managed Services use case page by clicking the Managed \
+ Services use case block in the SIP Trunking page', async () => {
+        await sipTrunkingProdPage.openManagedServUseCase()
+
+        await expect(browser).toHaveUrlContaining(/use-cases\/managed-services-telephony-reseller$/)
+        await expect(managedServCasePage.pageTitle()).toBeDisplayed()
+        await expect(managedServCasePage.pageTitle()).toHaveText('Managed Service Providers')
+        await expect(managedServCasePage.monUsePageSubtitle()).toBeDisplayed()
+        await expect(managedServCasePage.signUpTodayBtn()).toBeClickable()
+        await expect(managedServCasePage.seeApiPricingLink()).toBeClickable()
+    });
+
+    it('Should to open the Small Business use case page by clicking the Small Business\
+ use case block in the Trunking page', async () => {
+        await sipTrunkingProdPage.openSmallBusUseCase()
+
+        await expect(browser).toHaveUrlContaining(/use-cases\/voip-for-small-business$/)
+        await expect(smallBussCasePage.pageTitle()).toBeDisplayed()
+        await expect(smallBussCasePage.pageTitle()).toHaveText('The Best VoIP Solution for Small Business')
+        await expect(smallBussCasePage.voipVsLandlinePageSubtitle()).toBeDisplayed()
+        await expect(smallBussCasePage.signUpFor10Btn()).toBeClickable()
+    });
+
+    it('Should to open the Multi-Cloud use case page by clicking the Multi-Cloud\
+ use case block in the SIP Trunking page', async () => {
+        await sipTrunkingProdPage.openMultCloudUseCase()
+
+        await expect(browser).toHaveUrlContaining(/use-cases\/multi-cloud$/)
+        await expect(multiCloudCasePage.pageTitle()).toBeDisplayed()
+        await expect(multiCloudCasePage.pageTitle()).toHaveText('Multicloud')
+        
+        for (let i = 1; i < 5; i++) {
+            let buildProd = multiCloudCasePage.buildProduct(i)
+            await expect(buildProd).toBeClickable()
+        }
+
+        await expect(multiCloudCasePage.exploreDocLink()).toBeClickable()
+        await expect(multiCloudCasePage.downloadSheetLink()).toBeClickable()
+        await expect(multiCloudCasePage.tryFreeBtn()).toBeClickable()
+      });
+})
